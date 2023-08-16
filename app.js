e696ca7b1b5ed4d67b5e6b11f54626e440409071
Derek <nightwing917@hotmail.com>
2023-08-15 19:49:44 -0600

    trying text node message

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ alert("Connected!");
 
 const message = document.getElementById('messageSent');
 const p = document.createElement('p');
+const textNode = document.createTectNode('Message sent!');
 
 console.log(message);
