8bbaaa71824b43aac6336e2e32b294febe9e45ef
Derek <nightwing917@hotmail.com>
2023-08-17 19:30:28 -0600

    paragraph append child added with textnode

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ alert("Connected!");
 const message = document.getElementById('messageSent');
 const p = document.createElement('p');
 const textNode = document.createTectNode('Message sent!');
+p.appendChild(textNode);
+      
 
+      
 console.log(message);
