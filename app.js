23e3537aeff8c578a9962b7d4009af6a9c3221e1
Derek <nightwing917@hotmail.com>
2023-08-18 19:39:11 -0600

    adding appendChild

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const message = document.getElementById('messageSent');
 const p = document.createElement('p');
 const textNode = document.createTextNode('Message sent!');
 p.appendChild(textNode);
-      
+Element.appendChild(message, textNode);
+      /*const para = document.createElement("p");
+      const node = document.createTextNode("The end.");
+      para.appendChild(node);
+      const element = document.getElementById("div1");
+      const child = document.getElementById("p2");
+      element.appendChild(para,child);*/      
 
       
 console.log(message);
