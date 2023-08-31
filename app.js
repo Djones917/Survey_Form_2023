e34e12f63310d1289c20200abc8d8135e71e9fcb
Derek <nightwing917@hotmail.com>
2023-08-30 19:38:19 -0600

    moved event listener

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-alert("Connected!");
+/*alert("Connected!");*/
 
 const message = document.getElementById('messageSent');
-message.addEventListener('click', messageSent);
 const p = document.createElement('p');
 const textNode = document.createTextNode('Message sent!');
 p.appendChild(textNode);
 message.appendChild(textNode);
-          
+      
+
+
+message.addEventListener('click', messageSent);
 
 function messageSent() {
       alert('Message sent!');
