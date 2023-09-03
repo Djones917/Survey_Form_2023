392f5392feb98a636ee660dd53f6e775ddf3935a
Derek <nightwing917@hotmail.com>
2023-09-02 19:56:58 -0600

    playing around

--- app.js
+++ app.js
@@ -2,19 +2,21 @@
 
 const message = document.getElementById('messageSent');
 const p = document.createElement('p');
-/*p.style.display = none;*/
 const textNode = document.createTextNode('Message sent!');
 p.appendChild(textNode);
 message.appendChild(textNode);
-      
-
-
+      /*const para = document.createElement("p");
+      const node = document.createTextNode("The end.");
+      para.appendChild(node);
+      const element = document.getElementById("div1");
+      const child = document.getElementById("p2");
+      element.appendChild(para,child);*/      
 message.addEventListener('click', messageSent);
 
 function messageSent() {
-      alert('Message sent!');
+      message();
 }
 
       
-messageSent();
+messageSent();  
 /*console.log(message);*/
